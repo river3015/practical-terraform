0515a42ec519913caad1eff3a8e62070391729d2
river3015 <aaxy8402@yahoo.co.jp>
2024-12-16 21:51:40 +0900

    docker nginxイメージとコンテナデプロイ

---
+++ docker-nginx/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:stable-alpine"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = var.container_name
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
+
+variable "container_name" {
+  default     = "test"
+  type        = string
+  description = "The name of the container"
+}
